6c8ece9fb920a102bcada9ec29ede032395b94e5
Otavio Silva <otaviosil2018@hotmail.com>
2024-08-16 14:07:41 -0300

    metodo de localizacao do usuario foi modificada

--- lib/pages/map_page.dart
+++ lib/pages/map_page.dart
@@ -1,7 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:location/location.dart' as loc;
 import 'package:trashmap/widgets/recyclers/custom_app_bar_return.dart';
 
 class MapPage extends StatefulWidget {
@@ -13,45 +12,17 @@ class MapPage extends StatefulWidget {
 }
 
 class _MapPageState extends State<MapPage> {
-  final loc.Location location = loc.Location();
   late GoogleMapController _controller;
-  bool _added = false;
   Set<Marker> _markers = {};
   late BitmapDescriptor customMarker;
-  late BitmapDescriptor deviceLocationMarker;
+  late BitmapDescriptor homeLocationMarker;
+  bool _added = false;
 
   @override
   void initState() {
     super.initState();
-    _initializeMap();
     _loadCustomMarker();
-    _loadDeviceLocationMarker();
-  }
-
-  void _initializeMap() async {
-    await location.getLocation();
-    location.onLocationChanged.listen((loc.LocationData currentLocation) {
-      if (_controller != null) {
-        _controller.animateCamera(
-          CameraUpdate.newCameraPosition(
-            CameraPosition(
-              target: LatLng(currentLocation.latitude!, currentLocation.longitude!),
-              zoom: 14.47,
-            ),
-          ),
-        );
-
-        setState(() {
-          _markers.add(
-            Marker(
-              markerId: MarkerId('device_location'),
-              position: LatLng(currentLocation.latitude!, currentLocation.longitude!),
-              icon: deviceLocationMarker,
-            ),
-          );
-        });
-      }
-    });
+    _loadHomeLocationMarker();
   }
 
   Future<void> _loadCustomMarker() async {
@@ -61,10 +32,10 @@ class _MapPageState extends State<MapPage> {
     );
   }
   
-  Future<void> _loadDeviceLocationMarker() async {
-    deviceLocationMarker = await BitmapDescriptor.asset(
-      const ImageConfiguration(size: Size(48, 48)),
-      'assets/images/greenhomeicon.png',  
+  Future<void> _loadHomeLocationMarker() async {
+    homeLocationMarker = await BitmapDescriptor.asset(
+      ImageConfiguration(size: Size(48, 48)),
+      'assets/images/greenhomeicon.png',
     );
   }
 
@@ -73,27 +44,41 @@ class _MapPageState extends State<MapPage> {
     return Scaffold(
       appBar: customAppBarReturn(context, 'TRASHMAP'),
       body: StreamBuilder(
-        stream: FirebaseFirestore.instance.collection('location').snapshots(),
-        builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
-          if (_added) {
-            mapPage(snapshot);
-          }
+        stream: FirebaseFirestore.instance.collection('users').doc(widget.user_id).snapshots(),
+        builder: (context, AsyncSnapshot<DocumentSnapshot> snapshot) {
           if (!snapshot.hasData) {
             return const Center(child: CircularProgressIndicator());
           }
-          var userLocationData = snapshot.data!.docs.firstWhere(
-            (element) => element.id == widget.user_id,
-          );
-          _markers.add(
-            Marker(
-              position: LatLng(
-                userLocationData['latitude'],
-                userLocationData['longitude'],
+
+          var userLocationData = snapshot.data!;
+          if (!_added) {
+            _markers.add(
+              Marker(
+                position: LatLng(
+                  userLocationData['latitude'],
+                  userLocationData['longitude'],
+                ),
+                markerId: MarkerId('home_location'),
+                icon: homeLocationMarker,
               ),
-              markerId: MarkerId('user_location'),
-              icon: customMarker,
-            ),
-          );
+            );
+
+            _controller.animateCamera(
+              CameraUpdate.newCameraPosition(
+                CameraPosition(
+                  target: LatLng(
+                    userLocationData['latitude'],
+                    userLocationData['longitude'],
+                  ),
+                  zoom: 14.47,
+                ),
+              ),
+            );
+
+            setState(() {
+              _added = true;
+            });
+          }
 
           return GoogleMap(
             mapType: MapType.normal,
@@ -110,24 +95,6 @@ class _MapPageState extends State<MapPage> {
             },
           );
         },
-      ),
-    );
-  }
-
-  Future<void> mapPage(AsyncSnapshot<QuerySnapshot> snapshot) async {
-    await _controller.animateCamera(
-      CameraUpdate.newCameraPosition(
-        CameraPosition(
-          target: LatLng(
-            snapshot.data!.docs.singleWhere(
-              (element) => element.id == widget.user_id,
-            )['latitude'],
-            snapshot.data!.docs.singleWhere(
-              (element) => element.id == widget.user_id,
-            )['longitude'],
-          ),
-          zoom: 14.47,
-        ),
       ),
     );
   }
--- lib/pages/user_home_page.dart
+++ lib/pages/user_home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:trashmap/widgets/controllers/map_viewer.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class UserHomePage extends StatefulWidget {
   const UserHomePage({Key? key}) : super(key: key);
@@ -9,10 +11,109 @@ class UserHomePage extends StatefulWidget {
 }
 
 class _UserHomePageState extends State<UserHomePage> {
+  late GoogleMapController _mapController;
+  LatLng? _homeLocation;
+  bool _locationSaved = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _checkIfLocationIsSaved();
+  }
+
+  Future<void> _checkIfLocationIsSaved() async {
+    User? user = FirebaseAuth.instance.currentUser;
+    if (user != null) {
+      DocumentSnapshot userDoc = await FirebaseFirestore.instance
+          .collection('users')
+          .doc(user.uid)
+          .get();
+
+      if (userDoc.exists) {
+        setState(() {
+          _locationSaved = userDoc['latitude'] != null && userDoc['longitude'] != null;
+        });
+      }
+    }
+  }
+
+  void _onMapCreated(GoogleMapController controller) {
+    _mapController = controller;
+  }
+
+  void _onTap(LatLng location) {
+    setState(() {
+      _homeLocation = location;
+    });
+
+    _saveLocation(location);
+  }
+
+  Future<void> _saveLocation(LatLng location) async {
+    User? user = FirebaseAuth.instance.currentUser;
+    if (user != null) {
+      try {
+        // Update the user's document in Firestore with the selected location
+        await FirebaseFirestore.instance.collection('users').doc(user.uid).update({
+          'latitude': location.latitude,
+          'longitude': location.longitude,
+        });
+
+        setState(() {
+          _locationSaved = true;
+        });
+
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Location saved successfully!')),
+        );
+      } catch (e) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Failed to save location: $e')),
+        );
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: MapViewer(),
+      appBar: AppBar(
+        title: Text('Select Home Location'),
+        automaticallyImplyLeading: false, // Hide the back button
+      ),
+      body: Stack(
+        children: [
+          GoogleMap(
+            onMapCreated: _onMapCreated,
+            onTap: _locationSaved ? null : _onTap, // Disable tap after saving location
+            initialCameraPosition: CameraPosition(
+              target: LatLng(37.7749, -122.4194), // Default location (San Francisco)
+              zoom: 12,
+            ),
+            markers: _homeLocation != null
+                ? {
+                    Marker(
+                      markerId: MarkerId('home'),
+                      position: _homeLocation!,
+                    ),
+                  }
+                : {},
+          ),
+          if (_locationSaved)
+            Positioned(
+              bottom: 30,
+              left: 20,
+              right: 20,
+              child: ElevatedButton(
+                onPressed: () {
+                  // You can navigate to the next page or perform another action here
+                  Navigator.pushReplacementNamed(context, '/dashboard');
+                },
+                child: Text('Continue'),
+              ),
+            ),
+        ],
+      ),
     );
   }
 }
